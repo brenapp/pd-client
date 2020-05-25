9866c3f26ad6fcbde4f20f9f2cd5bd8e2dd6ddeb
Brendan McGuire <brendan@bren.app>
2020-05-24 22:00:20 -0400

    Custom rendering and slices

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Splash from "./routes/Splash";
+import { useRoutes } from "hookrouter";
+
+/**
+ * Routing
+ */
+const routes = {
+  // Splash Screen Procedures
+  "/": () => <Splash enteringCode={false} />,
+  "/join": (params: any) => <Splash enteringCode={true} />,
+  "/join/": (params: any) => <Splash enteringCode={true} />,
+  "/join/:code": (params: any) => (
+    <Splash code={params.code} enteringCode={true} />
+  ),
+};
 
 function App() {
+  const location = useRoutes(routes);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Fragment>
+      <header className="App-Header"></header>
+      <main>{location}</main>
+      <footer>
+        <div className="angled1 green"></div>
+        <div className="angled2 purple"></div>
+        <div className="bottom green"></div>
+      </footer>
+    </Fragment>
   );
 }
 
